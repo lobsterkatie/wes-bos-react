3e1ad7c340f7d68c6e52e9113059673da73b881f
Katie Byers <lobsterkatie@gmail.com>
2018-08-14 15:48:14 -0700

    finished through part 17

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,13 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = key => {
+    console.log(key);
+    const order = { ...this.state.order };
+    order[key] = (order[key] || 0) + 1;
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -30,11 +37,15 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder.bind(this, key)}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFish={this.loadSampleFish}
